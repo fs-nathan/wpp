cc142577fd93c7b48d4a4bc5a2155bdd0a2f3d8b
nghiem tien <tien.nghiembao@gmail.com>
2019-11-02 16:43:21 +0700

    update modal RemindTab

--- src/views/JobDetailPage/TabPart/RemindTab/TabBody/index.js
+++ src/views/JobDetailPage/TabPart/RemindTab/TabBody/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Avatar, IconButton, Menu, MenuItem } from '@material-ui/core';
+import {
+  Avatar, IconButton, Menu, MenuItem,
+  MenuList, Popper, Grow, Paper, ClickAwayListener
+} from '@material-ui/core';
 import Icon from '@mdi/react';
 import { mdiDotsVertical } from '@mdi/js';
 import ColorTypo from '../../../../../components/ColorTypo';
@@ -8,6 +11,7 @@ import ColorChip from '../../../../../components/ColorChip';
 import SearchInput from '../../../../../components/SearchInput';
 import avatar from '../../../../../assets/avatar.jpg';
 import colorPal from '../../../../../helpers/colorPalette';
+import RemindModal from '../RemindModal'
 
 
 const __data = [
@@ -15,6 +19,13 @@ const __data = [
   { badge: 'Đạt 55%', content: 'Liên hệ bộ phận đặt vật liệu đầu vào', title: 'Nhắc hẹn theo tiến độ thực tế' },
   { badge: 'Đạt 70%', content: 'Bộ phận marketing phải chăm sóc lại khách hàng', title: 'Nhắc hẹn theo tiến độ kế hoạch' },
   { badge: 'Trên 10%', content: 'Báo TGĐ để thịt', title: 'Tiến độ thực tế chậm so với kế hoạch' },
+];
+
+let mockDataEle = [
+  { badge: ['Hằng ngày'], content: 'Liên hệ chăm sóc khách hàng', title: 'Nhắc hẹn theo thời gian', date: '12/12/2012', time: '08:30' },
+  { badge: ['Đạt 55%'], content: 'Liên hệ bộ phận đặt vật liệu đầu vào', title: 'Nhắc hẹn theo tiến độ thực tế' },
+  { badge: ['Đạt 70%'], content: 'Bộ phận marketing phải chăm sóc lại khách hàng', title: 'Nhắc hẹn theo tiến độ kế hoạch' },
+  { badge: ['Trên 10%'], content: 'Báo TGĐ để thịt', title: 'Nhắc hẹn theo chênh lệch tiến độ hoàn thành giữa Kế hoạch - Thực tế' },
 ];
 
 const StyledList = styled.ul`
@@ -56,52 +67,86 @@ const Content = styled.div`
   justify-content: space-between;
 `
 
-const RemindList = () => {
-  const [data] = React.useState(__data);
+const MemberMenuLists = (props) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   function handleClick(evt) {
     setAnchorEl(evt.currentTarget)
   }
-  function handleClose() {
-    setAnchorEl(null);
-  }
 
+  function handleClose() {
+    setAnchorEl(null)
+  }
+  return (
+    <div>
+      <IconButton onClick={e => handleClick(e)} aria-controls={"simple-menu" + props.idx} aria-haspopup="true">
+        <Icon path={mdiDotsVertical} size={1} color={'rgba(0, 0, 0, 1)'} />
+      </IconButton>
+      <Menu
+            id={"simple-menu" + props.idx}
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            transformOrigin={{
+              vertical: -30,
+              horizontal: 'right',
+            }}
+          >
+            <MenuItem onClick={() => {
+              props.handleClickOpen(props.idx);
+              handleClose();
+              }}>Chỉnh sửa</MenuItem>
+            <MenuItem onClick={() => {handleClose()}}>Xóa</MenuItem>
+          </Menu> 
+
+    </div>
+  )
+}
+
+const RemindList = () => {
+
+  const [open, _setOpen] = React.useState(false);
+  const [elemState, _setElem] = React.useState({})
+
+  const [data] = React.useState(__data);
+  // Toogle popup array contains status of each popup
+  let arrOpens = mockDataEle.map(() => ({ isOpen: false }))
+  const [isOpens, setOpen] = React.useState(arrOpens);
+  const handleClickOpen = (elem) => {
+    _setOpen(true)
+    _setElem(elem)
+    
+  };
+  const handleClickClose = () => {
+    _setOpen(false)
+  };
+  
   return (
     <StyledList>
-      {data.map((elem, index) => {
+      {mockDataEle.map((elem, idx) => {
         return (
-          <StyledListItem key={index}>
+          <StyledListItem key={idx}>
             <Content>
               <StyledTitleBox>
                 <Avatar style={{ width: 25, height: 25 }} src={avatar} alt='avatar' />
                 <ColorTypo variant='body1'>{elem.title}</ColorTypo>
-                <ColorChip color='orangelight' size='small' badge label={elem.badge} />
+                {elem && elem.badge.map((item, key) => (
+                  <ColorChip key={key} color='orangelight' size='small' badge label={item + "" + key} />))
+                }
               </StyledTitleBox>
-              <IconButton onClick={handleClick} aria-controls="simple-menu" aria-haspopup="true">
-                <Icon path={mdiDotsVertical} size={1} color={'rgba(0, 0, 0, 1)'} />
-              </IconButton>
-              <Menu
-                id="simple-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-                transformOrigin={{
-                  vertical: -30,
-                  horizontal: 'right',
-                }}
-              >
-                <MenuItem onClick={handleClose}>Chỉnh sửa</MenuItem>
-                <MenuItem onClick={handleClose}>Xóa</MenuItem>
-              </Menu>
+
+              <MemberMenuLists idx={idx} handleClickOpen={() => handleClickOpen(elem)}/>
+
             </Content>
             <StyledContentBox>
               {elem.content}
             </StyledContentBox>
+            
           </StyledListItem>
         );
       })}
+      <RemindModal isOpen={open} handleClickClose={() => handleClickClose()} data={elemState} />
     </StyledList>
   );
 }
--- src/views/JobDetailPage/TabPart/RemindTab/TabHeader/index.js
+++ src/views/JobDetailPage/TabPart/RemindTab/TabHeader/index.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import { IconButton, Typography, Dialog, Button, TextField, withStyles } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { IconButton} from '@material-ui/core';
 import styled from 'styled-components';
 import Icon from '@mdi/react';
 import { mdiClose, mdiPlus } from '@mdi/js';
 import ColorTypo from '../../../../../components/ColorTypo';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import CloseIcon from '@material-ui/icons/Close';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import MuiDialogContent from '@material-ui/core/DialogContent';
-import MuiDialogActions from '@material-ui/core/DialogActions';
-
-
-const currencies = [
-  {
-    value: 'USD',
-    label: '$',
-  },
-  {
-    value: 'EUR',
-    label: '€',
-  },
-  {
-    value: 'BTC',
-    label: '฿',
-  },
-  {
-    value: 'JPY',
-    label: '¥',
-  },
-];
+import RemindModal from '../RemindModal'
 
 const Container = styled.div`
   display: flex;
@@ -44,199 +19,27 @@ const Container = styled.div`
   }
 `;
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    width: 160,
-  },
-}));
-
-const TitleText = styled(Typography)`
-  font-size: 15px;
-  margin: 20px 0
-`
-
-const TexTitle = styled(Typography)`
-  font-size: 15px;
-  width: 204px;
-`
-const HelperText = styled(TextField)`
-  & > *:last-child {
-    font-size: 12px;
-    margin: 8px 0 0;
-  }
-`
-const DivTitle = styled.div`
-  display: flex;
-  margin: 30px 0 10px 0;
-`
-
-const Div = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-const Text = styled(TextField)`
-  & > *:first-child {
-    margin-bottom: 20px;
-    & > input {
-      font-size: 16px;
-      margin-bottom: 100px;
-    }
-  }
-`
-
-const styles = theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
-  },
-});
-
-const DialogTitle = withStyles(styles)(props => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
-
-const DialogContent = withStyles(theme => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiDialogContent);
-
-const DialogActions = withStyles(theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
-
-
 function TabHeader({ setShow }) {
   // bien cua modal cong viec con
-  const [open, setOpen] = React.useState(false);
+  const [isOpen, setOpen] = React.useState(false);
   const handleClickOpen = () => {
     setOpen(true);
   };
   const handleClickClose = () => {
     setOpen(false);
   };
-  // bien menu item
-  const classes = useStyles();
-  const [currency, setCurrency] = React.useState('EUR');
 
-  const handleChange = event => {
-    setCurrency(event.target.value);
-  };
   return (
     <Container>
       <ColorTypo uppercase bold>Nhắc hẹn</ColorTypo>
-      <IconButton onClick={handleClickClose, handleClickOpen}>
+      <IconButton onClick={handleClickOpen}>
         <Icon path={mdiPlus} size={1} />
       </IconButton>
       <IconButton onClick={() => setShow(0)}>
         <Icon path={mdiClose} size={1} />
       </IconButton>
       {/* modal tao moi cong viec con */}
-      <Dialog aria-labelledby="customized-dialog-title" open={open} >
-        <DialogTitle id="customized-dialog-title" onClose={handleClickClose}>
-          Nhắc hẹn
-        </DialogTitle>
-        <DialogContent dividers>
-          <TitleText component="span">Loại nhắc hẹn</TitleText>
-          <HelperText component="span"
-            select
-            value={currency}
-            onChange={handleChange}
-            SelectProps={{
-              native: true,
-            }}
-            variant="outlined"
-            fullWidth
-            helperText="Bạn có lịch hẹn, ghi chú, sự kiện...quan trọng ? Hãy tạo nhắc hẹn theo thời gian để hệ thống nhắc nhở bạn khi đến hẹn."
-          >
-            {currencies.map(option => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </HelperText>
-          <Typography component="span">
-            <DivTitle component="span">
-              <TexTitle component="span">Ngày nhắc</TexTitle>
-              <TexTitle component="span">Giờ nhắc</TexTitle>
-              <Typography component="span" style={{ fontSize: 15 }}>Nhắc hẹn định kỳ</Typography>
-            </DivTitle>
-            <Div>
-              <TextField component="span"
-                placeholder="dd/mm/yyy"
-                className={classes.textField}
-                margin="normal"
-                variant="outlined"
-              />
-              <TextField component="span"
-                placeholder="hh/mm"
-                className={classes.textField}
-                margin="normal"
-                variant="outlined"
-              />
-              <TextField component="span"
-                select
-                className={classes.textField}
-                value={currency}
-                onChange={handleChange}
-                SelectProps={{
-                  native: true,
-                  MenuProps: {
-                    className: classes.menu,
-                  },
-                }}
-                margin="normal"
-                variant="outlined"
-              >
-                {currencies.map(option => (
-                  <option key={option.value} value={option.value}>
-                    {option.label}
-                  </option>
-                ))}
-              </TextField>
-            </Div>
-          </Typography>
-          <TitleText component="span">Nội dung</TitleText>
-          <Text component="span"
-          id="outlined-full-width"
-          placeholder="Nhập nội dung nhắc hẹn"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClickClose} color="primary">
-            Tạo nhắc hẹn
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <RemindModal isOpen={isOpen} handleClickClose={handleClickClose} />
     </Container>
   );
 }
