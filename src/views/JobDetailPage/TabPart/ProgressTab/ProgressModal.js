c89a7ce0d63780fca1ee673abfad2e57ca57e4ef
nghiem tien <tien.nghiembao@gmail.com>
2019-12-24 12:06:15 +0700

    fix css modal createjob

--- src/views/JobDetailPage/TabPart/ProgressTab/ProgressModal.js
+++ src/views/JobDetailPage/TabPart/ProgressTab/ProgressModal.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import { IconButton, withStyles, Typography, Dialog, Button } from '@material-ui/core';
+import { IconButton, withStyles, Typography, Dialog, Button, TextField } from '@material-ui/core';
 import styled from 'styled-components';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
+// import OutlinedInput from '@material-ui/core/OutlinedInput';
 import CloseIcon from '@material-ui/icons/Close';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
-import TimeField from 'react-simple-timefield';
+// import TimeField from 'react-simple-timefield';
 // import OutlinedInputSelect from './OutlinedInputSelect'
 import { WrapperContext } from '../../index';
-
+import {
+  DEFAULT_DATE_TEXT, DEFAULT_END_TIME_TEXT, DEFAULT_START_TIME_TEXT
+} from '../../../../helpers/jobDetail/stringHelper';
 const StartEndDay = styled(Typography)`
   display: flex;
   flex-direction: row;
   align-items: center;
   margin: 30px 0;
 `
-const StartEndDate = styled(Typography)`
-  margin: 0 20px;
-`
-const OutlineInput = styled(OutlinedInput)`
-& > input {
-  padding: 10px 5px;
-  margin-left: 10px;
-}
-`
+// const StartEndDate = styled(Typography)`
+//   margin: 0 20px;
+// `
+
 const BeginEndTime = styled(Typography)`
   width: 50px;
   margin-right: 20px;
@@ -39,20 +36,18 @@ const BeginEndTime = styled(Typography)`
 //   flex-direction: row;
 //   align-items: center;
 // `
-const InputTime = styled(TimeField)`
-  width: 146px !important;
-  padding: 10px 5px 10px 13px;
-  border: 0;
-  border-radius: 4px;
-`
 const DivTime = styled.div`
-  border: 1px solid #cfcfcf;
-  border-radius: 4px;
 `
 // const InputSelect = styled(OutlinedInputSelect)`
 //   width: 331px;
 // `
-
+const InputDateTime = styled(TextField)`
+  width: 186px;
+  margin-right: 48px;
+  & > div:nth-child(2) > input {
+    padding: 14px;
+  }
+`
 const styles = theme => ({
   root: {
     margin: 0,
@@ -102,10 +97,10 @@ const DialogActions = withStyles(theme => ({
 const ProgressModal = (props) => {
   const value = React.useContext(WrapperContext)
   // console.log("value time:::::", value);
-  const [startTime, setStartTime] = React.useState("")
-  const [endTime, setEndTime] = React.useState("")
-  const [startDay, setStartDay] = React.useState("")
-  const [endDay, setEndDay] = React.useState("")
+  const [startTime, setStartTime] = React.useState(DEFAULT_START_TIME_TEXT)
+  const [endTime, setEndTime] = React.useState(DEFAULT_END_TIME_TEXT)
+  const [startDay, setStartDay] = React.useState(DEFAULT_DATE_TEXT)
+  const [endDay, setEndDay] = React.useState(DEFAULT_DATE_TEXT)
 
   const handleStartTime = (startTime) => {
     setStartTime(startTime)
@@ -119,7 +114,6 @@ const ProgressModal = (props) => {
   const handleEndDay = (endDay) => {
     setEndDay(endDay)
   }
-
 
   const setDataTimeDuration = () => {
     const data = {
@@ -142,7 +136,7 @@ const ProgressModal = (props) => {
         Điều chỉnh tiến độ
         </DialogTitle>
       <DialogContent dividers style={{ overflowY: 'hidden' }}>
-        <StartEndDay component={'span'}>
+        {/* <StartEndDay component={'span'}>
           <BeginEndTime component={'span'}>Bắt đầu</BeginEndTime>
           <DivTime>
             <InputTime value={startTime} onChange={(e) => handleStartTime(e.target.value)} />
@@ -157,7 +151,46 @@ const ProgressModal = (props) => {
           </DivTime>
           <StartEndDate component={'span'}>Ngày</StartEndDate>
           <OutlineInput type={'date'} value={endDay} onChange={(e) => handleEndDay(e.target.value)} />
-        </StartEndDay>
+        </StartEndDay> */}
+        <StartEndDay component={'span'}>
+            <BeginEndTime component={'span'}>Bắt đầu</BeginEndTime>
+            <DivTime>
+              <InputDateTime
+                type={'time'}
+                label="Thời gian"
+                variant="outlined"
+                value={startTime} 
+                onChange={(e) => handleStartTime(e.target.value)}
+              />
+            </DivTime>
+            <InputDateTime
+              label="Ngày"
+              variant="outlined"
+              type={'date'}
+              value={startDay} 
+              onChange={(e) => handleStartDay(e.target.value)}
+            />
+          </StartEndDay>
+          <StartEndDay component={'span'}>
+            <BeginEndTime component={'span'}>Kết thúc</BeginEndTime>
+            <DivTime>
+              <InputDateTime
+                type={'time'}
+                label="Thời gian"
+                variant="outlined"
+                value={endTime} 
+                onChange={(e) => handleEndTime(e.target.value)}
+              />
+            </DivTime>
+            <InputDateTime
+              label="Ngày"
+              variant="outlined"
+              type={'date'}
+              inputProps={{ min: startDay }}
+              value={endDay} 
+              onChange={(e) => handleEndDay(e.target.value)}
+            />
+          </StartEndDay>
       </DialogContent>
       <DialogActions>
         <Button onClick={() => {
