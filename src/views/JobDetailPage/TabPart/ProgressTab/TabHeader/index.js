8f771a4a8b339e70b74db625789bf321e2902eca
antn <antn9x@gmail.com>
2020-03-10 09:16:21 +0700

    fix header progress

--- src/views/JobDetailPage/TabPart/ProgressTab/TabHeader/index.js
+++ src/views/JobDetailPage/TabPart/ProgressTab/TabHeader/index.js
@@ -1,22 +1,13 @@
 import React, { useEffect } from 'react';
-import { IconButton} from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import styled from 'styled-components';
 import Icon from '@mdi/react';
-import { mdiChevronLeft , mdiSettings } from '@mdi/js';
+import { mdiChevronLeft, mdiSettings } from '@mdi/js';
 import ColorTypo from '../../../../../components/ColorTypo';
 import ProgressModal from '../ProgressModal'
 import { useSelector, useDispatch } from 'react-redux';
 import { taskIdSelector } from '../../../selectors';
 import { getTrackingTime } from '../../../../../actions/taskDetail/taskDetailActions';
-// const Container = styled.div`
-//   display: flex;
-//   align-items: center;
-//   justify-content: space-between;
-//   background-color: #fff;
-//   border-bottom: 1px solid rgba(0, 0, 0, .1);
-//   height: 85px;
-//   overflow-y: hidden;
-// `;
 
 const ButtonIcon = styled(IconButton)`
   &:hover {
@@ -31,6 +22,7 @@ const ButtonIcon = styled(IconButton)`
 
 const ProgressTitle = styled(ColorTypo)`
   font-size: 17px;
+  font-weight: normal;
 `
 
 function TabHeader({ setShow }) {
@@ -41,7 +33,7 @@ function TabHeader({ setShow }) {
   useEffect(() => {
     dispatch(getTrackingTime(taskId))
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+  }, [])
   const [open, setOpen] = React.useState(false);
   const handleClickOpen = () => {
     setOpen(true);
@@ -51,13 +43,17 @@ function TabHeader({ setShow }) {
   };
   return (
     <div className="container-progress-tabheader">
-      <ButtonIcon onClick={() => setShow(0)}>
-        <Icon path={mdiChevronLeft } size={1} />
-      </ButtonIcon>
-      <ProgressTitle uppercase bold>Tiến độ công việc</ProgressTitle>
-      <ButtonIcon onClick={handleClickOpen}>
-        <Icon path={mdiSettings} size={1} />
-      </ButtonIcon>
+      <abbr title="Quay lại">
+        <ButtonIcon onClick={() => setShow(0)}>
+          <Icon path={mdiChevronLeft} size={1} />
+        </ButtonIcon>
+      </abbr>
+      <ProgressTitle uppercase >Tiến độ công việc</ProgressTitle>
+      <abbr title="Cài đặt">
+        <ButtonIcon onClick={handleClickOpen}>
+          <Icon path={mdiSettings} size={1} />
+        </ButtonIcon>
+      </abbr>
       {/* modal tao moi cong viec con */}
       <ProgressModal isOpen={open} handleClickOpen={handleClickOpen} handleClickClose={handleClickClose} />
     </div>
