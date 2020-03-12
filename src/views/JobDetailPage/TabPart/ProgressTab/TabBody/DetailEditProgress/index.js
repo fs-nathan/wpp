cf107a6bda765ca1303abc39a2328b522ce340ac
antn <antn9x@gmail.com>
2020-03-12 18:07:05 +0700

    fix progress tab

---
+++ src/views/JobDetailPage/TabPart/ProgressTab/TabBody/DetailEditProgress/index.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import clsx from 'classnames';
+import { mdiTimelineClockOutline, mdiMenuDown } from '@mdi/js';
+import Icon from '@mdi/react'
+
+import EditProgressItem from './EditProgressItem';
+
+import './styles.scss'
+
+function DetailEditProgress({ trackings }) {
+  const [showDetail, setShowDetail] = useState(false);
+
+  function toggleDetail() {
+    setShowDetail(!showDetail)
+  }
+
+  return (<div className="detailEditProgress">
+    <div className="detailEditProgress--header">
+      <Icon
+        path={mdiTimelineClockOutline}
+        color="rgba(0, 0, 0, 0.54)"
+        size={1}
+      />
+      <div className="detailEditProgress--change">
+        Điểu chỉnh tiến độ: {trackings.length} lần
+    <div className="detailEditProgress--detail" onClick={toggleDetail}>
+          {showDetail ? 'Thu gọn lịch sử điều chỉnh tiến độ' : 'Xem lịch sử điều chỉnh tiến độ'}
+          <Icon
+            path={mdiMenuDown}
+            color="rgba(0, 0, 0, 0.54)"
+            size={1}
+            className={clsx({ 'detailEditProgress__expanded': showDetail })}
+          />
+        </div>
+      </div>
+    </div>
+    {showDetail && trackings.map((track, i) => (<EditProgressItem
+      key={i}
+      fixedNumber={i + 1}
+      fixStart={track.new_start}
+      fixEnd={track.new_end}
+      createdAt={track.time_create}
+      avatarUrl={track.user_create_avatar}
+    // userName={track.user_created}
+    ></EditProgressItem>))}
+  </div>)
+}
+
+export default DetailEditProgress;
--- src/views/JobDetailPage/TabPart/ProgressTab/TabBody/index.js
+++ src/views/JobDetailPage/TabPart/ProgressTab/TabBody/index.js
@@ -1,50 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Avatar, Table, TableHead, TableBody, TableRow, TableCell } from '@material-ui/core';
 import { mdiCircle } from '@mdi/js';
 import Icon from '@mdi/react'
 import { Scrollbars } from 'react-custom-scrollbars'
 import { useSelector, useDispatch } from 'react-redux';
 import ColorTypo from '../../../../../components/ColorTypo';
-import colorPal from '../../../../../helpers/colorPalette';
 import { taskIdSelector } from '../../../selectors';
 import { updateComplete } from '../../../../../actions/taskDetail/taskDetailActions';
 import ProgressSlider from './ProgressSlider';
+import DetailEditProgress from './DetailEditProgress';
 
-const BlueTableCell = styled(TableCell)`
-  color: ${colorPal['blue'][0]};
-`;
-
-const CellAvatar = styled(TableCell)`
-  padding-left: 0;
-`
-
-const TypoTitle = styled(ColorTypo)`
-  color: ${colorPal['gray'][0]};
-  margin-left: 20px;
-`
-const TableHistory = styled(Table)`
-  margin-left: 20px;
-  & > *:first-child {
-    
-  }
-`
-const UserAvatar = styled(Avatar)`
-    width: 30px;
-    height: 30px;
-  `
-
-const TableRowItem = styled(TableRow)`
-  border-bottom: 1px dashed grey;
-  & > th, td {
-    border-bottom: none;
-  }
-`
-
-const Body = styled(Scrollbars)`
-  grid-area: body;
-  height: 100%;
-`;
+import './styles.scss'
 
 function TabBody() {
   const dispatch = useDispatch();
@@ -53,29 +18,8 @@ function TabBody() {
   const listTime = useSelector(state => state.taskDetail.trackingTime.listTime);
   const projectId = useSelector(state => state.taskDetail.commonTaskDetail.activeProjectId);
 
-  let listTimeRender = null;
-
-  if (listTime && listTime.trackings) {
-    listTimeRender = listTime.trackings.map((item, key) => {
-      return (
-        <TableRowItem key={key}>
-          <CellAvatar>
-            <UserAvatar src={item.user_create_avatar} alt='avatar' />
-          </CellAvatar>
-          <TableCell>
-            Lần {key + 1}
-            <p className="red-table-cell">{item.time_create}</p>
-          </TableCell>
-          <BlueTableCell>
-            Bắt đầu: {item.new_start}
-            <br />
-            Kết thúc: {item.new_end}
-          </BlueTableCell>
-        </TableRowItem>
-      )
-    })
-  }
-
+  const trackings = listTime ? listTime.trackings : [];
+  const [firstTrack] = trackings
   const onChangeCommitted = (data) => {
     let task_id = taskId
     let complete = parseFloat(data);
@@ -83,7 +27,7 @@ function TabBody() {
   }
 
   return (
-    <Body autoHide autoHideTimeout={500} autoHideDuration={200}>
+    <Scrollbars className="progressTabBody" autoHide autoHideTimeout={500} autoHideDuration={200}>
       <div className="container-progress-tabbody">
         <div className="start-end-date-box">
           <div className="start-date-box">
@@ -103,28 +47,17 @@ function TabBody() {
         />
         <div className="legend-box">
           <Icon path={mdiCircle} size={1} color={'#2dc63a'} />
-          <ColorTypo>Hoàn thành thực tế</ColorTypo>
+          <ColorTypo>Tiến độ hoàn thành
+          {firstTrack && <span className="progressTabBody--recent">(cập nhật gần đây {firstTrack.time_create})</span>}
+          </ColorTypo>
         </div>
         <div className="legend-box">
           <Icon path={mdiCircle} size={1} color={'#ff9800'} />
-          <ColorTypo>Kế hoạch</ColorTypo>
+          <ColorTypo>Tiến độ kế hoạch đến ngày hiện tại (dự kiến)</ColorTypo>
         </div>
-        {/* progress end */}
-        <TypoTitle bold variant='subtitle1' >Lịch sử điều chỉnh tiến độ</TypoTitle>
-        <TableHistory style={{ marginLeft: 20, marginRight: 20 }}>
-          <TableHead>
-            <TableRowItem>
-              <TableCell></TableCell>
-              <TableCell>Lần thứ</TableCell>
-              <TableCell>Nội dung điều chỉnh</TableCell>
-            </TableRowItem>
-          </TableHead>
-          <TableBody>
-            {listTimeRender}
-          </TableBody>
-        </TableHistory>
+        <DetailEditProgress trackings={trackings} />
       </div>
-    </Body>
+    </Scrollbars>
   )
 }
 
