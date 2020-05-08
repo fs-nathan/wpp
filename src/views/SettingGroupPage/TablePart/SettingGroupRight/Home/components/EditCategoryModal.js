384d6e672ea9e1854c0b287d3a5c21c7ef1ad1bf
longnh01 <ngohoanglongptit@gmail.com>
2020-05-08 22:22:29 +0700

    update

--- src/views/SettingGroupPage/TablePart/SettingGroupRight/Home/components/EditCategoryModal.js
+++ src/views/SettingGroupPage/TablePart/SettingGroupRight/Home/components/EditCategoryModal.js
@@ -1,73 +1,15 @@
 import Joi from "@hapi/joi";
-import { Box } from "@material-ui/core";
-import { listIcon } from "actions/icon/listIcon";
-import { Field, Formik, FormikContext } from "formik";
+import { Field, Formik } from "formik";
 import React, { useCallback, useContext, useEffect, useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { useDispatch } from "react-redux";
 import { useToggle } from "react-use";
-import LogoManager from "views/DepartmentPage/Modals/LogoManager";
-import ModalCommon from "views/DocumentPage/TablePart/DocumentComponent/ModalCommon";
-import { DialogContent } from "views/DocumentPage/TablePart/DocumentComponent/TableCommon";
-import VerticleList from "views/JobPage/components/VerticleList";
+import LogoManagerModal from "views/DepartmentPage/Modals/LogoManager";
 import { emptyObject } from "views/JobPage/contants/defaultValue";
 import HomeContext from "../HomeContext";
-import { createPostCategory } from "../redux";
+import { updatePostCategory } from "../redux";
 import { apiCallStatus } from "../redux/apiCall/types";
 import useAsyncTracker from "../redux/apiCall/useAsyncTracker";
-import {
-  InputFormControl,
-  MultilineInputFormControl,
-  SelecIconInputFormControl,
-} from "./CssFormControl";
-export const CategoryModal = ({
-  title = "Tạo thể loại",
-  loading,
-  onClose,
-  toggleLogoModal,
-}) => {
-  const { t } = useTranslation();
-  const { handleSubmit } = useContext(FormikContext);
-  return (
-    <ModalCommon
-      loading={loading}
-      title={t(title)}
-      onClose={onClose}
-      footerAction={[
-        {
-          action: handleSubmit,
-          name: t("Hoàn thành"),
-        },
-      ]}
-    >
-      <DialogContent dividers className="dialog-content move-content">
-        <Box padding="24px">
-          <VerticleList>
-            <InputFormControl name="name" label={t("Tên thể loại")} />
-            <MultilineInputFormControl
-              name="description"
-              label={t("Mô tả chi tiết")}
-            />
-            <SelecIconInputFormControl
-              name="logoPreview"
-              onClick={toggleLogoModal}
-              label={t("Biểu tượng")}
-              addLabel={t("Chọn biểu tượng")}
-            ></SelecIconInputFormControl>
-          </VerticleList>
-        </Box>
-      </DialogContent>
-    </ModalCommon>
-  );
-};
-const LogoManagerModal = (props) => {
-  const dispath = useDispatch();
-  useEffect(() => {
-    dispath(listIcon());
-  }, [dispath]);
-  return <LogoManager {...props} />;
-};
-const addCategoryFormInitialValues = { name: "", description: "", logo: "" };
+import { CategoryModal } from "./AddCategotyModal";
 
 const createValidate = (schema) => (values = {}, mapError = {}) => {
   const { error } = schema.validate(values);
@@ -78,7 +20,7 @@ const createValidate = (schema) => (values = {}, mapError = {}) => {
       }, {})
     : emptyObject;
 };
-const validateAddCategoryForm = createValidate(
+const validateEditCategoryForm = createValidate(
   Joi.object({
     name: Joi.string().required(),
     logo: Joi.any(),
@@ -86,27 +28,31 @@ const validateAddCategoryForm = createValidate(
     description: Joi.any(),
   })
 );
-const AddCategoryForm = ({ children, onSubmit }) => {
+const EditCategoryForm = ({
+  initialValues = emptyObject,
+  children,
+  onSubmit,
+}) => {
   // error.details[0].type
   const { t } = useTranslation();
   const validateMemo = useMemo(
     () => (values = {}) => {
       const mapError = { "string.empty": t("required") };
-      return validateAddCategoryForm(values, mapError);
+      return validateEditCategoryForm(values, mapError);
     },
     [t]
   );
   return (
     <Formik
-      initialValues={addCategoryFormInitialValues}
+      initialValues={initialValues}
       onSubmit={onSubmit}
-      validate={validateMemo}
+      // validate={validateMemo}
     >
       {children}
     </Formik>
   );
 };
-export default () => {
+export default ({ id, logo, name }) => {
   const { setModal } = useContext(HomeContext);
   const [{ status }, setAsyncAction] = useAsyncTracker();
   const onClose = useCallback(() => {
@@ -116,9 +62,12 @@ export default () => {
     status === apiCallStatus.success && onClose();
   }, [onClose, status]);
   const [openLogoModal, toggleLogoModal] = useToggle();
-  const handleSubmit = (values) => setAsyncAction(createPostCategory(values));
+  const handleSubmit = (values) => setAsyncAction(updatePostCategory(values));
   return (
-    <AddCategoryForm onSubmit={handleSubmit}>
+    <EditCategoryForm
+      initialValues={{ category_id: id, logoPreview: logo, logo, name }}
+      onSubmit={handleSubmit}
+    >
       <>
         <CategoryModal
           loading={status === apiCallStatus.loading}
@@ -151,6 +100,6 @@ export default () => {
           </Field>
         )}
       </>
-    </AddCategoryForm>
+    </EditCategoryForm>
   );
 };
