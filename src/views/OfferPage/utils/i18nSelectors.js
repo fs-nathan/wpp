a6a245955d59201b6099fc284259f63be65232ae
Tri Le <lmtri1995@gmail.com>
2020-05-17 22:52:44 +0700

    chore: add i18n keys selectors for popover options of offer table items

--- src/views/OfferPage/utils/i18nSelectors.js
+++ src/views/OfferPage/utils/i18nSelectors.js
@@ -3,3 +3,9 @@ const detailOfferPath = `${offerPagePath}.DETAIL_OFFER`;
 export const getDetailOfferModalTitle = t => t(`${detailOfferPath}.TITLE`);
 export const getDetailOfferModalConfirmBtnTitle = t => t(`${detailOfferPath}.DELETE_OFFER`);
 export const getDetailOfferModalCancelBtnTitle = t => t(`${detailOfferPath}.ESCAPE`);
+
+const offerTablePath = `${offerPagePath}.OFFER_TABLE`;
+const offerTableItemPath = `${offerTablePath}.ITEM`;
+const offerTableItemPopoverPath = `${offerTableItemPath}.POPOVER`;
+export const getOfferTableItemPopoverDetail = t => t(`${offerTableItemPopoverPath}.DETAIL`);
+export const getOfferTableItemPopoverDelete = t => t(`${offerTableItemPopoverPath}.DELETE`);
