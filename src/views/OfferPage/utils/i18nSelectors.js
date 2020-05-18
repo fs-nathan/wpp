5dfe6089d96f47207beeca11347a8b079781d3a7
Tri Le <lmtri1995@gmail.com>
2020-05-18 08:04:33 +0700

    docs: add section comments to i18n selectors

--- src/views/OfferPage/utils/i18nSelectors.js
+++ src/views/OfferPage/utils/i18nSelectors.js
@@ -1,9 +1,11 @@
+// Detail Offer
 const offerPagePath = 'OFFER_PAGE';
 const detailOfferPath = `${offerPagePath}.DETAIL_OFFER`;
 export const getDetailOfferModalTitle = t => t(`${detailOfferPath}.TITLE`);
 export const getDetailOfferModalConfirmBtnTitle = t => t(`${detailOfferPath}.DELETE_OFFER`);
 export const getDetailOfferModalCancelBtnTitle = t => t(`${detailOfferPath}.ESCAPE`);
 
+// Offer Table Item Popover
 const offerTablePath = `${offerPagePath}.OFFER_TABLE`;
 const offerTableItemPath = `${offerTablePath}.ITEM`;
 const offerTableItemPopoverPath = `${offerTableItemPath}.POPOVER`;
