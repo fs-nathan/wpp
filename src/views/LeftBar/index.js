c48ad237e228560c815e545ecb4b6196121b6c72
longnh01 <ngohoanglongptit@gmail.com>
2020-07-08 00:07:04 +0700

    update

--- src/views/LeftBar/index.js
+++ src/views/LeftBar/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
 import {
   actionVisibleDrawerMessage,
-  openNoticeModal
-} from '../../actions/system/system';
-import { Routes } from '../../constants/routes';
+  openNoticeModal,
+} from "../../actions/system/system";
+import { Routes } from "../../constants/routes";
+import { isEmpty } from "../../helpers/utils/isEmpty";
 // import { useTranslation } from 'react-i18next';
 // import * as icons from '../../assets';
-import './LeftBar.scss';
-import { isEmpty } from '../../helpers/utils/isEmpty';
+import "./LeftBar.scss";
 
-const isDocument = type => {
+const isDocument = (type) => {
   switch (type) {
     case Routes.DOCUMENT_RECENT:
     case Routes.DOCUMENT_PROJECT:
@@ -33,36 +33,36 @@ const LeftBar = ({
   groupActive,
   profile,
   actionVisibleDrawerMessage,
-  openNoticeModal
+  openNoticeModal,
 }) => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   const pathname = history.location.pathname;
   const { group_active } = profile;
-  const bgColor = colors.find(item => item.selected === true);
+  const bgColor = colors.find((item) => item.selected === true);
   const onCloseDrawer = () => {
-    actionVisibleDrawerMessage({ type: '', anchor: anchorDrawer });
+    actionVisibleDrawerMessage({ type: "", anchor: anchorDrawer });
   };
   let menuList = [];
   let itemManage = false;
   if (!isEmpty(group_active) && !isEmpty(group_active.modules)) {
     group_active.modules.forEach((el, idx) => {
-      const isActived = pathname.indexOf(el.path_search) == 0 ? true : false
+      const isActived = pathname.indexOf(el.path_search) == 0 ? true : false;
       if (el.is_manage_group) {
         itemManage = {
           ...el,
           isSelected: isActived,
-          icon: isActived ? el.url_icon_selected : el.url_icon
-        }
+          icon: isActived ? el.url_icon_selected : el.url_icon,
+        };
       } else {
         menuList.push({
           ...el,
           isSelected: isActived,
-          icon: isActived ? el.url_icon_selected : el.url_icon
+          icon: isActived ? el.url_icon_selected : el.url_icon,
         });
       }
     });
   }
-  const isFree = groupActive.type === 'Free';
+  const isFree = groupActive.type === "Free";
   return (
     <div className="left-bar-container" style={{ background: bgColor.color }}>
       {!isEmpty(menuList) &&
@@ -71,7 +71,7 @@ const LeftBar = ({
             <Link
               to={isFree ? null : el.url_redirect}
               key={idx}
-              className={`menu-item ${el.isSelected ? 'actived' : ''}`}
+              className={`menu-item ${el.isSelected ? "actived" : ""}`}
               onClick={() => {
                 if (isFree) {
                   openNoticeModal();
@@ -80,7 +80,7 @@ const LeftBar = ({
               }}
             >
               <img src={el.icon} alt="" className="LeftNavIcon" />
-              <span className="titleTab">{el.name}</span>
+              <span className="titleTab">{t(el.name)}</span>
             </Link>
           );
         })}
@@ -88,14 +88,12 @@ const LeftBar = ({
         <Link
           to={itemManage.url_redirect}
           onClick={onCloseDrawer}
-          className={`menu-item menu-item-last ${itemManage.isSelected ? 'actived' : ''}`}
+          className={`menu-item menu-item-last ${
+            itemManage.isSelected ? "actived" : ""
+          }`}
           title={itemManage.name}
         >
-          <img
-            src={itemManage.icon}
-            alt=""
-            className="LeftNavIcon"
-          />
+          <img src={itemManage.icon} alt="" className="LeftNavIcon" />
         </Link>
       )}
     </div>
@@ -103,11 +101,11 @@ const LeftBar = ({
 };
 
 export default connect(
-  state => ({
+  (state) => ({
     colors: state.setting.colors,
     anchorDrawer: state.system.anchorDrawer,
     groupActive: state.system.groupActive,
-    profile: state.system.profile
+    profile: state.system.profile,
   }),
   { actionVisibleDrawerMessage, openNoticeModal }
 )(withRouter(LeftBar));
