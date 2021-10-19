cdb24bbfbf4f64e6df6a3122ade6c4921f8d989e
Pham_Tinh <phamvu.tinh99@gmail.com>
2021-10-19 17:59:18 +0700

    update page chat

--- src/views/ProjectGroupPage/index.js
+++ src/views/ProjectGroupPage/index.js
@@ -1,11 +1,15 @@
 import { makeStyles } from "@material-ui/styles";
 import { getPermissionViewProjects } from "actions/viewPermissions";
 import classNames from "classnames";
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect } from "react";
 import { connect } from "react-redux";
 import { useLocation } from "react-router";
 import { Route, Switch } from "react-router-dom";
 import { useLocalStorage } from "react-use";
+import GranttPage from "views/GrantPage/GrantTable";
+import ChatPage from "views/JobDetailPage";
+import KanbanPage from "views/KanbanPage";
+import HeaderRightPart from "views/ProjectGroupPage/RightPart/components/HeaderRightPart.js";
 import {
   checkHasRecentlyProjects,
   countPersonalProjectsBoard,
@@ -61,60 +65,69 @@ function ProjectGroupPage({
   const _handleExpand = () => setIsCollapsed(!isCollapsed);
 
   return (
-    <Route path={route}>
-      <div className={classNames(classes.wrapper, { isCollapsed })}>
-        {!isCollapsed && (
-          <div className={classNames(classes.leftSidebar, { isCollapsed })}>
-            {isDeletedPage ? <ProjectGroupListDeleted /> : <ProjectGroupList />}
-          </div>
-        )}
-        <div className={classNames(classes.mainContent, { isCollapsed })}>
-          <Switch>
-            <Route exact path="/projects/recently">
-              <AllProjectTable
-                type_data={1}
-                expand={isCollapsed}
-                handleExpand={_handleExpand}
-              />
-            </Route>
-            <Route exact path="/projects/start">
-              <ProjectsStart
-                expand={isCollapsed}
-                handleExpand={_handleExpand}
-              />
-            </Route>
-            <Route exact path="/projects/personal-board">
-              <AllProjectTable
-                type_data={2}
-                expand={isCollapsed}
-                handleExpand={_handleExpand}
-              />
-            </Route>
-            <Route exact path="/projects/group/:projectGroupId">
-              <AllProjectTable
-                expand={isCollapsed}
-                handleExpand={_handleExpand}
-              />
-            </Route>
-            <Route exact path="/projects/deleted">
-              <DeletedProjectTable
-                expand={isCollapsed}
-                handleExpand={_handleExpand}
-              />
-            </Route>
-            <Route exact path="/projects/task-table/:projectId/:memberId?">
-              <AllTaskTable expand={isCollapsed} handleExpand={_handleExpand} />
-            </Route>
-            <Route exact path="/projects">
-              <AllProjectTable
-                expand={isCollapsed}
-                handleExpand={_handleExpand}
-              />
-            </Route>
-          </Switch>
+    <div className={classNames(classes.wrapper, { isCollapsed })}>
+      {!isCollapsed && (
+        <div className={classNames(classes.leftSidebar, { isCollapsed })}>
+          {isDeletedPage ? <ProjectGroupListDeleted /> : <ProjectGroupList />}
         </div>
+      )}
+      <div className={classNames(classes.mainContent, { isCollapsed })}>
+        <HeaderRightPart />
+        <Switch>
+          <Route exact path="/projects/recently">
+            <AllProjectTable
+              type_data={1}
+              expand={isCollapsed}
+              handleExpand={_handleExpand}
+            />
+          </Route>
+          <Route exact path="/projects/start">
+            <ProjectsStart expand={isCollapsed} handleExpand={_handleExpand} />
+          </Route>
+          <Route exact path="/projects/personal-board">
+            <AllProjectTable
+              type_data={2}
+              expand={isCollapsed}
+              handleExpand={_handleExpand}
+            />
+          </Route>
+          <Route exact path="/projects/group/:projectGroupId">
+            <AllProjectTable
+              expand={isCollapsed}
+              handleExpand={_handleExpand}
+            />
+          </Route>
+          <Route exact path="/projects/deleted">
+            <DeletedProjectTable
+              expand={isCollapsed}
+              handleExpand={_handleExpand}
+            />
+          </Route>
+
+          {/* Detail project */}
+          <Route exact path="/projects/task-table/:projectId/:memberId?">
+            <AllTaskTable expand={isCollapsed} handleExpand={_handleExpand} />
+          </Route>
+          <Route exact path="/projects/task-kanban/:projectId/:memberId?">
+            <KanbanPage />
+          </Route>
+          <Route exact path="/projects/task-gantt/:projectId/:memberId?">
+            <GranttPage />
+          </Route>
+          <Route exact path="/projects/task-chat/:projectId/:memberId?">
+            <ChatPage />
+          </Route>
+          {/* Detail project */}
+
+          <Route exact path="/projects">
+            <AllProjectTable
+              expand={isCollapsed}
+              handleExpand={_handleExpand}
+            />
+          </Route>
+        </Switch>
       </div>
-    </Route>
+    </div>
   );
 }
 
