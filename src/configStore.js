b8e04a4455c9789937e7249c60c6341245591d31
Quan Tran <quantran2381@gmail.com>
2022-03-28 17:14:15 +0700

    add redux logger

--- src/configStore.js
+++ src/configStore.js
@@ -3,13 +3,15 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer, { DEFAULT_STATE } from './reducers';
 import rootSaga from './sagas';
 
+// Logger with default options
+import logger from 'redux-logger';
 
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   DEFAULT_STATE,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
+  composeEnhancers(applyMiddleware(logger, sagaMiddleware))
 );
 sagaMiddleware.run(rootSaga);
 
