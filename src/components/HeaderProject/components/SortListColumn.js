24aee5a3a08b8e7c837b1ebb31f459330e52063d
Pham_Tinh <phamvu.tinh99@gmail.com>
2022-03-29 21:21:31 +0700

    Update sortlist column

--- src/components/HeaderProject/components/SortListColumn.js
+++ src/components/HeaderProject/components/SortListColumn.js
@@ -1,28 +1,85 @@
-import {
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Switch,
-} from "@material-ui/core";
-import { isArray } from "lodash";
-import RootRef from "@material-ui/core/RootRef";
+import { ListItemIcon, ListItemText, Switch } from "@material-ui/core";
 import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
+import { FixedSizeList, areEqual } from "react-window";
 import { listColumns } from "actions/columns/listColumns";
+import { isArray } from "lodash";
 import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
-const reorder = (list, startIndex, endIndex) => {
+const marginBottom = 4;
+const marginTop = 4;
+const marginLeft = 15;
+const marginRight = 5;
+
+function reorder(list, startIndex, endIndex) {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
-
   return result;
-};
+}
+
+function getStyle({ provided, style, isDragging }) {
+  const combined = {
+    ...style,
+    ...provided.draggableProps.style,
+    width: 335,
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  };
+
+  const withSpacing = {
+    ...combined,
+    height: isDragging
+      ? combined.height
+      : combined.height - (marginBottom + marginTop),
+    width: isDragging
+      ? combined.width
+      : combined.width - (marginLeft + marginRight),
+    marginBottom,
+    marginLeft,
+    marginRight,
+    marginTop,
+  };
+  return withSpacing;
+}
+
+function Item({ provided, item, style, isDragging }) {
+  return (
+    <StyledItem
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
+      ref={provided.innerRef}
+      style={getStyle({ provided, style, isDragging })}
+      className={`item ${isDragging ? "is-dragging" : ""}`}
+    >
+      <ListItemIcon style={{ minWidth: "20px" }}>
+        <StyledIconDrag />
+      </ListItemIcon>
+      <ListItemText primary={item.name} style={{ color: "#1e1f21" }} />
+      <AntSwitch
+        defaultChecked={item.is_show}
+        style={{ marginRight: 5 }}
+        inputProps={{ "aria-label": "ant design" }}
+      />
+    </StyledItem>
+  );
+}
+
+// Recommended react-window performance optimisation: memoize the row render function
+// Things are still pretty fast without this, but I am a sucker for making things faster
+const Row = React.memo(function Row(props) {
+  const { data: items, index, style } = props;
+  const item = items[index];
+  return (
+    <Draggable draggableId={item.id} index={index} key={item.id}>
+      {(provided) => <Item provided={provided} item={item} style={style} />}
+    </Draggable>
+  );
+}, areEqual);
 
 const SortListColumn = React.forwardRef((props, ref) => {
   const { projectId } = useParams();
@@ -58,77 +115,34 @@ const SortListColumn = React.forwardRef((props, ref) => {
   if (!isArray(items)) return null;
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable">
-        {(provided, snapshot) => (
-          <RootRef rootRef={provided.innerRef}>
-            <List style={getListStyle(snapshot.isDraggingOver)}>
-              {items.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
-                  {(provided, snapshot) => (
-                    <ListItem
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(
-                        snapshot.isDragging,
-                        provided.draggableProps.style
-                      )}
-                    >
-                      <ListItemIcon style={{ minWidth: "20px" }}>
-                        <DragIndicatorIcon
-                          style={{
-                            width: "18px",
-                            height: "18px",
-                            fill: "#6d6e6f",
-                          }}
-                        />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={item.name}
-                        style={{ color: "#1e1f21" }}
-                      />
-                      <AntSwitch
-                        defaultChecked={item.is_show}
-                        style={{ marginRight: 5 }}
-                        inputProps={{ "aria-label": "ant design" }}
-                      />
-                    </ListItem>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </List>
-          </RootRef>
+      <Droppable
+        droppableId="droppable_sorting"
+        mode="virtual"
+        renderClone={(provided, snapshot, rubric) => (
+          <Item
+            provided={provided}
+            isDragging={snapshot.isDragging}
+            item={items[rubric.source.index]}
+          />
+        )}
+      >
+        {(provided) => (
+          <FixedSizeList
+            height={500}
+            width={350}
+            maxWidth={330}
+            itemCount={items.length}
+            itemSize={55}
+            outerRef={provided.innerRef}
+            itemData={items}
+            style={{ paddingTop: "10px", height: "auto" }}
+          >
+            {Row}
+          </FixedSizeList>
         )}
       </Droppable>
     </DragDropContext>
   );
-});
-
-const getItemStyle = (isDragging, draggableStyle) => ({
-  // styles we need to apply on draggables
-  ...draggableStyle,
-  ...(isDragging && {}),
-  borderRadius: 5,
-  border: "1px solid rgba(0, 0, 0, 0.1)",
-  color: "#666",
-  marginBottom: 10,
-  "& > div": {
-    paddingLeft: 0,
-  },
-  boxShadow: "0 0 0 1px #edeae9, 0 1px 4px 0 rgb(109 110 111 / 8%)",
-  borderColor: "#fff",
-  alignItems: "center",
-  display: "flex",
-  justifyContent: "space-between",
-  marginTop: "8px",
-  padding: "8px 4px",
-});
-
-const getListStyle = (isDraggingOver) => ({
-  paddingLeft: 16,
-  paddingRight: 16,
-  paddingBottom: 0,
 });
 
 export const AntSwitch = styled(Switch)(({ theme }) => ({
@@ -175,4 +189,30 @@ export const AntSwitch = styled(Switch)(({ theme }) => ({
   },
 }));
 
-export default SortListColumn
+const StyledIconDrag = styled(DragIndicatorIcon)`
+  width: 18px;
+  height: 18px;
+  fill: #6d6e6f;
+`;
+
+const StyledItem = styled.div`
+  box-shadow: rgb(237 234 233) 0px 0px 0px 1px,
+    rgb(109 110 111 / 8%) 0px 1px 4px 0px;
+  box-sizing: border-box;
+  border-radius: 8px;
+  font-size: 30px;
+  user-select: none;
+  padding-right: 5px;
+
+  /* center align text */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: "#fff";
+
+  &.is-dragging {
+    height: 37px;
+  }
+`;
+
+export default SortListColumn;
