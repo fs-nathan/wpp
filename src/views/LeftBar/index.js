97c3f847e9e03cc2610f1a52879d52e5f399c47b
Quan Tran <quantran2381@gmail.com>
2022-04-01 10:13:07 +0700

    menu top

--- src/views/LeftBar/index.js
+++ src/views/LeftBar/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { connect } from "react-redux";
-import { Link, withRouter } from "react-router-dom";
+import { useLocation, Link, withRouter } from "react-router-dom";
 import ReactHtmlParser from "react-html-parser";
 import { Button } from "@material-ui/core";
 import Badge from "@material-ui/core/Badge";
@@ -19,6 +19,13 @@ import { isEmpty } from "../../helpers/utils/isEmpty";
 import { useSelector, useDispatch } from "react-redux";
 // import { useTranslation } from 'react-i18next';
 // import * as icons from '../../assets';
+import AddUserModal from "views/DepartmentPage/Modals/AddUserModal";
+import ModalCreateAccount from "components/CustomTable/Modal/create-account";
+import ModalOptionCreateAccount from "components/CustomTable/Modal/optionCreateAccount";
+import CreateAccountModal from "views/DepartmentPage/Modals/CreateAccount";
+import ModalResultCreateAccount from "components/CustomTable/Modal/result-create-account";
+import ModalContinueCreateAccount from "components/CustomTable/Modal/continue-create-account";
+import ModalUplaodExcel from "components/CustomTable/Modal/uploadExcel";
 
 import "./LeftBar.scss";
 import { getNumberMessageNotView } from "actions/chat/threadChat";
@@ -31,6 +38,8 @@ import UpdateIcon from "@mui/icons-material/Update";
 import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import backgroundmenu from "assets/home_baner_right1.png";
+import GroupAccountModal from "components/GroupAccountModal/GroupAccountModal";
+
 const BellMessage = () => {
   const numberChatNotView = useSelector(
     (state) => state.threadChat.numberChatNotView
@@ -100,6 +109,7 @@ const child_menu_top = [
     path_search_more: false,
     path_search: "/setting-group/info",
     url_redirect: "/setting-group/info",
+    isSettingGroup: true,
     icon: UpdateIcon,
   },
   {
@@ -127,6 +137,7 @@ const child_menu_top = [
     background: backgroundmenu,
     url_redirect: "/setting-group/create-order",
     isBackground: true,
+    isCreateModal: true,
   },
 ];
 
@@ -238,7 +249,7 @@ const LeftBar = ({
   };
 
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+  const openAnchor = Boolean(anchorEl);
   const handleClose = () => {
     setAnchorEl(null);
   };
@@ -246,131 +257,216 @@ const LeftBar = ({
     setAnchorEl(event.currentTarget);
   };
 
+  const onClickMenu = (childItem) => {
+    if (childItem.isSettingGroup) {
+      setVisibleGroupAccountModal(true);
+      handleClose();
+      return;
+    }
+    if (childItem.isCreateModal) {
+      setOpenAddUserModal(true);
+      handleClose();
+      return;
+    }
+    history.push(childItem.url_redirect);
+  };
+  const location = useLocation();
+  React.useEffect(() => {
+    if (location.pathname !== currentRoute) {
+      setAnchorEl(null);
+      setRoute(location.pathname);
+    }
+  }, [location.pathname]);
+
+  const [currentRoute, setRoute] = React.useState(location.pathname);
+
+  const [openCreateAccount, setOpenCreateAccount] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [openAddUSerModal, setOpenAddUserModal] = React.useState(false);
+  const [openContinueCreateAccount, setOpenContinueCreateAccount] =
+    React.useState(false);
+  const [openCreateAccountModal, setOpenCreateAccountModal] =
+    React.useState(false);
+  const [result, setResult] = React.useState(null);
+  const [openResultCreateAccount, setOpenResultCreateAccount] =
+    React.useState(false);
+  const [fileExcel, setFileExcel] = React.useState("");
+  const [openUploadExcel, setOpenUploadExcel] = React.useState(false);
+  const handlesetFileExcel = (file) => {
+    setFileExcel(file);
+  };
+  const [visibleGroupAccountModal, setVisibleGroupAccountModal] =
+    useState(false);
   return (
-    <div
-      className={`left-bar-container`}
-      style={{
-        background: bgColor.color,
-      }}
-    >
-      <div>
-        <div
-          className="logo menu-item"
-          // onClick={() => setVisibleGroupModal(true)}
-          onClick={handleClick}
-          variant="logo-avatar"
-          aria-controls={open ? "demo-customized-menu" : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? "true" : undefined}
-        >
-          <div className="menu-icon">{logo && <img src={logo} />}</div>
-          <div className="logo-name">
-            <p className="title">{detailProfile.name}</p>
-            <p className="id">ID: {detailProfile.code}</p>
+    <>
+      {visibleGroupAccountModal && (
+        <GroupAccountModal
+          open={visibleGroupAccountModal}
+          setOpen={(val) => setVisibleGroupAccountModal(val || false)}
+          props
+        />
+      )}
+      <CreateAccountModal
+        open={openCreateAccountModal}
+        setOpen={setOpenCreateAccountModal}
+      />
+      <ModalCreateAccount
+        setOpenAddMember={setOpenAddUserModal}
+        openAddMember={openAddUSerModal}
+        setOpen={setOpen}
+        setOpenCreateAccount={setOpenCreateAccount}
+        setOpenContinueCreateAccount={setOpenContinueCreateAccount}
+      />
+      <AddUserModal setOpen={setOpen} open={open} />
+      <ModalOptionCreateAccount
+        openCreateAccount={openCreateAccount}
+        setOpenCreateAccount={setOpenCreateAccount}
+        setOpenContinueCreateAccount={setOpenContinueCreateAccount}
+      />
+      <ModalContinueCreateAccount
+        fileExcel={fileExcel}
+        setResult={setResult}
+        setOpenResultCreateAccount={setOpenResultCreateAccount}
+        openContinueCreateAccount={openContinueCreateAccount}
+        setOpenContinueCreateAccount={setOpenContinueCreateAccount}
+        setOpenUploadExcel={setOpenUploadExcel}
+      />
+      <ModalResultCreateAccount
+        result={result}
+        openResultCreateAccount={openResultCreateAccount}
+        setOpenResultCreateAccount={setOpenResultCreateAccount}
+      />
+      <ModalUplaodExcel
+        handlesetFileExcel={handlesetFileExcel}
+        openUploadExcel={openUploadExcel}
+        setOpenUploadExcel={setOpenUploadExcel}
+        setOpenContinueCreateAccount={setOpenContinueCreateAccount}
+      />
+      <div
+        className={`left-bar-container`}
+        style={{
+          background: bgColor.color,
+        }}
+      >
+        <div>
+          <div
+            className="logo menu-item"
+            // onClick={() => setVisibleGroupModal(true)}
+            onClick={handleClick}
+            variant="logo-avatar"
+            aria-controls={open ? "demo-customized-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            <div className="menu-icon">{logo && <img src={logo} />}</div>
+            <div className="logo-name">
+              <p className="title">{detailProfile.name}</p>
+              <p className="id">ID: {detailProfile.code}</p>
+            </div>
           </div>
-        </div>
-        <StyledMenu
-          id="demo-customized-menu"
-          MenuListProps={{
-            "aria-labelledby": "demo-customized-button",
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-        >
-          {child_menu_top.map((childItem, index) => {
-            return (
-              <MenuItem
-                key={`${index}${childItem.name}`}
-                disableRipple
-                onClick={() => history.push(childItem.url_redirect)}
-                className={`${
-                  childItem.isBackground ? "background-boolean" : ""
-                }`}
-              >
-                <div className="menu-icon-avatar--popover">
-                  <span className="icon">
-                    {childItem?.icon && (
-                      <childItem.icon
-                        style={{
-                          width: "25px",
-                          height: "25px",
-                          fill: "#6d6e6f",
-                        }}
-                      />
-                    )}
-                    {/* {childItem.isBackground && (
+          <StyledMenu
+            id="demo-customized-menu"
+            MenuListProps={{
+              "aria-labelledby": "demo-customized-button",
+            }}
+            anchorEl={anchorEl}
+            open={openAnchor}
+            onClose={handleClose}
+          >
+            {child_menu_top.map((childItem, index) => {
+              return (
+                <MenuItem
+                  key={`${index}${childItem.name}`}
+                  disableRipple
+                  onClick={() => onClickMenu(childItem)}
+                  className={`${
+                    childItem.isBackground ? "background-boolean" : ""
+                  }`}
+                >
+                  <div className="menu-icon-avatar--popover">
+                    <span className="icon">
+                      {childItem?.icon && (
+                        <childItem.icon
+                          style={{
+                            width: "25px",
+                            height: "25px",
+                            fill: "#6d6e6f",
+                          }}
+                        />
+                      )}
+                      {/* {childItem.isBackground && (
                       <img src={childItem.isBackground} />
                     )} */}
-                  </span>
-                  {childItem.isBackground ? (
-                    <span
-                      className="background-apply-text"
-                      style={{ padding: 0, margin: 0 }}
-                      dangerouslySetInnerHTML={{ __html: childItem.name }}
-                    >
-                      {/* {childItem.name} */}
                     </span>
-                  ) : (
-                    childItem.name
-                  )}
-                </div>
-              </MenuItem>
-            );
-          })}
-        </StyledMenu>
-        {menuList.map((item, index) => {
-          return item.need_bell ? (
-            <Link
-              key={index}
-              className={`menu-item ${item.isSelected ? "actived" : ""}`}
-              to={item.url_redirect}
-            >
-              <Badge
-                badgeContent={props.newMessage > 0 ? "" : null}
-                color="error"
-                className={`menu-badge-leftbar ${
-                  props.newMessage <= 0 ? "none-view" : ""
-                }`}
+                    {childItem.isBackground ? (
+                      <span
+                        className="background-apply-text"
+                        style={{ padding: 0, margin: 0 }}
+                        dangerouslySetInnerHTML={{ __html: childItem.name }}
+                      >
+                        {/* {childItem.name} */}
+                      </span>
+                    ) : (
+                      childItem.name
+                    )}
+                  </div>
+                </MenuItem>
+              );
+            })}
+          </StyledMenu>
+          {menuList.map((item, index) => {
+            return item.need_bell ? (
+              <Link
+                key={index}
+                className={`menu-item ${item.isSelected ? "actived" : ""}`}
+                to={item.url_redirect}
+              >
+                <Badge
+                  badgeContent={props.newMessage > 0 ? "" : null}
+                  color="error"
+                  className={`menu-badge-leftbar ${
+                    props.newMessage <= 0 ? "none-view" : ""
+                  }`}
+                >
+                  <div className="menu-icon">
+                    {item.icon && <img src={item.icon.default} />}
+                  </div>
+                </Badge>
+                <p>{t(item.name)}</p>
+              </Link>
+            ) : item.isCheckServer ? (
+              <Link
+                key={index}
+                className={`menu-item ${item.isSelected ? "actived" : ""}`}
+                to={findurlDirect(item)}
               >
                 <div className="menu-icon">
                   {item.icon && <img src={item.icon.default} />}
                 </div>
-              </Badge>
-              <p>{t(item.name)}</p>
-            </Link>
-          ) : item.isCheckServer ? (
-            <Link
-              key={index}
-              className={`menu-item ${item.isSelected ? "actived" : ""}`}
-              to={findurlDirect(item)}
-            >
-              <div className="menu-icon">
-                {item.icon && <img src={item.icon.default} />}
-              </div>
-              <p>{t(item.name)}</p>
-            </Link>
-          ) : (
-            <Link
-              key={index}
-              className={`menu-item ${item.isSelected ? "actived" : ""}`}
-              to={item.url_redirect}
-            >
-              <div className="menu-icon">
-                {item.icon && <img src={item.icon.default} />}
-              </div>
-              <p>{t(item.name)}</p>
-            </Link>
-          );
-        })}
-      </div>
+                <p>{t(item.name)}</p>
+              </Link>
+            ) : (
+              <Link
+                key={index}
+                className={`menu-item ${item.isSelected ? "actived" : ""}`}
+                to={item.url_redirect}
+              >
+                <div className="menu-icon">
+                  {item.icon && <img src={item.icon.default} />}
+                </div>
+                <p>{t(item.name)}</p>
+              </Link>
+            );
+          })}
+        </div>
 
-      <div>
-        {bottomList.map((item, index) => {
-          return <PopoverMenu key={index} {...item} profile={profileState} />;
-        })}
+        <div>
+          {bottomList.map((item, index) => {
+            return <PopoverMenu key={index} {...item} profile={profileState} />;
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
