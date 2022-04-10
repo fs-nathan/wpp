345f1bc0201fdb02b7d5f050ef036d0d4604cf8f
Eddie <eddie@devx.com.au>
2022-04-10 16:51:36 +0700

    feat: :sparkles: create share feature UI done

--- src/views/ProjectGroupPage/RightPart/ProjectAddNew/index.js
+++ src/views/ProjectGroupPage/RightPart/ProjectAddNew/index.js
@@ -11,7 +11,7 @@ const STEP = {
 };
 
 const ProjectAddNew = ({ handleExpand }) => {
-  const [step, setStep] = useState(STEP.CREATE);
+  const [step, setStep] = useState(STEP.INITIAL);
 
   useEffect(() => {
     handleExpand();
