1f0363eb45f0154534a5230e50c37c3456c32d1f
Eddie <eddie@devx.com.au>
2022-04-23 14:01:27 +0700

    fix: :bug: blank page

--- src/views/ProjectGroupPage/RightPart/ProjectsTemplate/components/TemplateSection/TemplateSection.js
+++ src/views/ProjectGroupPage/RightPart/ProjectsTemplate/components/TemplateSection/TemplateSection.js
@@ -8,6 +8,7 @@ import TemplateCard from "../TemplateCard/TemplateCard";
 import "./index.scss";
 
 const TemplateSection = ({
+  isMainPage = false,
   categoryId,
   icon,
   title,
@@ -19,11 +20,22 @@ const TemplateSection = ({
 
   return (
     <div className="template-group__section">
-      <div className="template-group__section__title">
-        <span>{icon}</span>
-        <h3>{title}</h3>
-        <div>{extra && extra}</div>
-      </div>
+      {isMainPage ? (
+        templates &&
+        templates.length > 0 && (
+          <div className="template-group__section__title">
+            <span>{icon}</span>
+            <h3>{title}</h3>
+            <div>{extra && extra}</div>
+          </div>
+        )
+      ) : (
+        <div className="template-group__section__title">
+          <span>{icon}</span>
+          <h3>{title}</h3>
+          <div>{extra && extra}</div>
+        </div>
+      )}
 
       <div className="template-group__section__card">
         {templates &&
