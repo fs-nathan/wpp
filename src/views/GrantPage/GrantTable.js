8c0d87fdf76f4a3b207e77ddf8952fd98b7801e5
Khac Diep <dieptk95@gmail.com>
2022-04-29 13:43:36 +0700

    Fix gantt

--- src/views/GrantPage/GrantTable.js
+++ src/views/GrantPage/GrantTable.js
@@ -1835,7 +1835,7 @@ class DragSortingTable extends React.Component {
                   <Table
                     columns={colShow}
                     size="small"
-                    className="table-gantt-header"
+                    className="table-gantt-header gantt-header-custom"
                     bordered
                     {...scroll}
                     rowClassName={(record, index) => {
